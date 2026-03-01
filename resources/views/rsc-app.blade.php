47c975ecc1193678981e91bfa1be737127ddc003
Ramon Malcolm <ramonmalcolm10@gmail.com>
2026-03-01 08:54:19 -0500

    Add modulepreload hint for hydration entry in base RSC view

--- resources/views/rsc-app.blade.php
+++ resources/views/rsc-app.blade.php
@@ -1,8 +1,12 @@
 <!DOCTYPE html>
-<html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    @php($hydrateEntry = collect(glob(public_path('build/rsc/entry.hydrate-*.js')))->first())
+    @if($hydrateEntry)
+    <link rel="modulepreload" href="/build/rsc/{{ basename($hydrateEntry) }}">
+    @endif
 </head>
 <body>
     <div id="rsc-root">{!! $body !!}</div>
